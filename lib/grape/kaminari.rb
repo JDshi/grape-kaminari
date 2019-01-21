49b0d6e8227d046f4576c9576847a34940488099
weizhen <openness666@gmail.com>
2019-01-21 22:03:05 +0800

    direct return pagination

--- lib/grape/kaminari.rb
+++ lib/grape/kaminari.rb
@@ -10,13 +10,17 @@ module Grape
         helpers do
           def paginate(collection)
             collection.page(params[:page].to_i).per(params[:per_page].to_i).padding(params[:offset].to_i).tap do |data|
-              header "X-Total",       data.total_count.to_s
-              header "X-Total-Pages", data.total_pages.to_s
-              header "X-Per-Page",    data.limit_value.to_s
-              header "X-Page",        data.current_page.to_s
-              header "X-Next-Page",   data.next_page.to_s
-              header "X-Prev-Page",   data.prev_page.to_s
-              header "X-Offset",      params[:offset].to_s
+              return {
+                       'pagination': {
+                          'X-Total': data.total_count.to_s,
+                          'X-Total-Pages': data.total_pages.to_s,
+                          'X-Per-Page': data.limit_value.to_s,
+                          'X-Page': data.current_page.to_s,
+                          'X-Next-Page': data.next_page.to_s,
+                          'X-Prev-Page': data.prev_page.to_s,
+                          'X-Offset': params[:offset].to_s
+                        }
+                    }
             end
           end
         end
